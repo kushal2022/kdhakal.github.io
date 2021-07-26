dfb5452eac8c84d85ca064fef62f567d8e9af505
kushal2022 <39408734+kushal2022@users.noreply.github.com>
2021-07-26 11:03:24 -0500

    KJDS
    FEA

--- staticfiles/W1D6animationAndDecorate/js/ascii.js
+++ staticfiles/W1D6animationAndDecorate/js/ascii.js
@@ -1,50 +1,9 @@
-"use strict";
-var textArea, animDdl, sizeDdl, timer, startButton, stopButton, speed = 250, turboChk;
-
-function start() {
-    // var frames = textArea.val().split("=====\n");
-    var frames = textArea.value.split("=====\n");
-    var i = 0, l = frames.length;
-    (function iterator() {
-        textArea.value = frames[i];
-
-        if (++i >= l) {
-            i = 0;
-        }
-        timer = setTimeout(iterator, speed);
-    })();
-    stopButton.disabled = false;
-    startButton.disabled = true;
-};
-
-function stop(time) {
-    stopButton.disabled = true;
-    startButton.disabled = false;
-    clearTimeout(timer);
-    getFrames();
-}
-
-
-function getFrames() {
-    var dropSel = animDdl.options[animDdl.selectedIndex].innerHTML;
-    textArea.value = ANIMATIONS[dropSel];
-
-}
-
-function setSize() {
-    textArea.style.fontSize = sizeDdl.options[sizeDdl.selectedIndex].value;
-}
-
-function turbo() {
-    if (turboChk.checked) {
-        speed = 50;
-    }
-    else {
-        speed = 250;
-    }
-}
+import ANIMATIONS from "../JS/animation.js";
 
 window.onload = function () {
+    "use strict";
+    var textArea, animDdl, sizeDdl, timer, startButton, stopButton, speed = 250, turboChk;
+
     textArea = document.getElementById("text-area");
     animDdl = document.getElementById("animation");
     sizeDdl = document.getElementById("fontsize");
@@ -58,5 +17,48 @@ window.onload = function () {
     animDdl.onchange = getFrames;
     sizeDdl.onchange = setSize;
     turboChk.onchange = turbo;
+
+    function start() {
+        // var frames = textArea.val().split("=====\n");
+        var frames = textArea.value.split("=====\n");
+        var i = 0, l = frames.length;
+        (function iterator() {
+            textArea.value = frames[i];
+
+            if (++i >= l) {
+                i = 0;
+            }
+            timer = setTimeout(iterator, speed);
+        })();
+        stopButton.disabled = false;
+        startButton.disabled = true;
+    }
+
+    function stop() {
+        stopButton.disabled = true;
+        startButton.disabled = false;
+        clearTimeout(timer);
+        getFrames();
+    }
+
+
+    function getFrames() {
+        var dropSel = animDdl.options[animDdl.selectedIndex].innerHTML;
+        textArea.value = ANIMATIONS[dropSel];
+
+    }
+
+    function setSize() {
+        textArea.style.fontSize = sizeDdl.options[sizeDdl.selectedIndex].value;
+    }
+
+    function turbo() {
+        if (turboChk.checked) {
+            speed = 50;
+        }
+        else {
+            speed = 250;
+        }
+    }
 
 };
