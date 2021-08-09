6c7b5df17ca03c9c54a8b493f7faf27dbdf7c64f
kushal2022 <39408734+kushal2022@users.noreply.github.com>
2021-08-09 09:59:33 -0500

    moduleexpert
    this is for controller

--- staticfiles/W3D6NodeEJStemplate/question3_4/controllers/shoppingCartController.js
+++ staticfiles/W3D6NodeEJStemplate/question3_4/controllers/shoppingCartController.js
@@ -1,9 +1,9 @@
+const CARTS = [
+    {name: 'Dell laptop', price: 7000, quantity: 3},
+    {name: 'Dell monitor', price: 3000, quantity: 4},
+];
 module.exports ={
     get: (req, res) => {
-        const CARTS = [
-            {name: 'Dell laptop', price: 7000, quantity: 3},
-            {name: 'Dell monitor', price: 3000, quantity: 4},
-        ];
         res.render('shoppingCart', {
             product: CARTS
         });
